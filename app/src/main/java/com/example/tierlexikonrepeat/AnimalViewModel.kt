234b26f2f762a318acb66ce1b0ae16a7937a3da6
luc <luc.spielvogel@syntax-institut.de>
2023-12-19 10:22:37 +0100

    viewmodel

---
+++ app/src/main/java/com/example/tierlexikonrepeat/AnimalViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.tierlexikonrepeat
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.tierlexikonrepeat.data.Animal
+import com.example.tierlexikonrepeat.data.Repository
+
+class AnimalViewModel : ViewModel() {
+    private val repository = Repository()
+    private var animals = repository.getAnimals
+
+    private val _animalList: MutableLiveData<List<Animal>> = MutableLiveData(animals)
+    val animalList : LiveData<List<Animal>>
+        get() = _animalList
+
+    private val _currentAnimal: MutableLiveData<Animal> = MutableLiveData()
+    val currentAnimal : LiveData<Animal>
+        get() = _currentAnimal
+
+    fun setCurrentAnimal(animal: Animal) {
+        _currentAnimal.value = animal
+    }
+}
